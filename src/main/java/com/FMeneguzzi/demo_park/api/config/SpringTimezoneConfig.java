9280a8d9a3472196d72a3d88cbdd2db17aba4033
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-01-22 20:25:42 -0300

    Configuração BD e JPA

--- src/main/java/com/FMeneguzzi/demo_park/api/config/SpringTimezoneConfig.java
+++ src/main/java/com/FMeneguzzi/demo_park/api/config/SpringTimezoneConfig.java
@@ -1,5 +1,17 @@
 package com.FMeneguzzi.demo_park.api.config;
 
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
 public class SpringTimezoneConfig {
+	
+	@PostConstruct
+	public void timezoneConfig() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 }
