5c7efd4eb1fbc9035e68a09d4c08d6c24a7f71ab
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-01-22 22:02:35 -0300

    Criar Usuario

--- src/main/java/com/FMeneguzzi/demo_park/api/service/UserService.java
+++ src/main/java/com/FMeneguzzi/demo_park/api/service/UserService.java
@@ -2,12 +2,20 @@ package com.FMeneguzzi.demo_park.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.FMeneguzzi.demo_park.api.entities.User;
 import com.FMeneguzzi.demo_park.api.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 	private UserRepository userRepository;
+
+	@Transactional
+	public User salvar(User user) {
+		return userRepository.save(user);
+		
+	}
 }
--- src/main/java/com/FMeneguzzi/demo_park/api/web/controller/UserController.java
+++ src/main/java/com/FMeneguzzi/demo_park/api/web/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.FMeneguzzi.demo_park.api.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.FMeneguzzi.demo_park.api.entities.User;
 import com.FMeneguzzi.demo_park.api.service.UserService;
 
 //import lombok.RequiredArgsConstructor;
@@ -15,6 +20,13 @@ public class UserController {
 
 	@Autowired
     private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+    	User user1 = userService.salvar(user);
+    	return ResponseEntity.status(HttpStatus.CREATED).body(user1);
+
+    }
 
 }
 
