5e245b85933ea3d7db8b494334d54ae3ab9a6325
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-01-24 19:49:30 -0300

    configuração camada de testes

---
+++ src/test/resources/sql/user/user-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS(id, username, password, role) values (100, 'a@email.com', 'ROLE_ADMIN');
+insert into USUARIOS(id, username, password, role) values (200, 'b@email.com', 'ROLE_ADMIN');
+insert into USUARIOS(id, username, password, role) values (300, 'c@email.com', 'ROLE_CLIENT');
