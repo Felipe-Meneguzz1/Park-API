f99683a92d0dd1886bdfef1a12e21fa0d023a882
Felipe-Meneguzz1 <lipemeneguzzi@gmail.com>
2025-01-28 23:46:29 -0300

    Metodo para buscar pelo Id e documentação

--- src/main/java/com/FMeneguzzi/demo_park/api/service/ClienteService.java
+++ src/main/java/com/FMeneguzzi/demo_park/api/service/ClienteService.java
@@ -1,7 +1,9 @@
 package com.FMeneguzzi.demo_park.api.service;
 
 import com.FMeneguzzi.demo_park.api.entities.Cliente;
+import com.FMeneguzzi.demo_park.api.entities.User;
 import com.FMeneguzzi.demo_park.api.exception.CpfUniqueViolationException;
+import com.FMeneguzzi.demo_park.api.exception.EntityNotFoundException;
 import com.FMeneguzzi.demo_park.api.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -21,5 +23,12 @@ public class ClienteService {
         } catch (DataIntegrityViolationException ex) {
             throw new CpfUniqueViolationException(String.format("CPF '%s' não pode ser cadastrado, já existe no sistema", cliente.getCpf()));
         }
+    }
+
+    @Transactional
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Cliente com '%s' não encontrado", id))
+        );
     }
 }
--- src/main/java/com/FMeneguzzi/demo_park/api/web/controller/ClienteControle.java
+++ src/main/java/com/FMeneguzzi/demo_park/api/web/controller/ClienteControle.java
@@ -6,17 +6,22 @@ import com.FMeneguzzi.demo_park.api.service.ClienteService;
 import com.FMeneguzzi.demo_park.api.service.UserService;
 import com.FMeneguzzi.demo_park.api.web.dto.ClienteCreateDto;
 import com.FMeneguzzi.demo_park.api.web.dto.ClienteResponseDto;
+import com.FMeneguzzi.demo_park.api.web.dto.UserResponseDto;
 import com.FMeneguzzi.demo_park.api.web.dto.mapper.ClienteMapper;
+import com.FMeneguzzi.demo_park.api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Cliente", description = "Contem todas as operações relativas os recursos de um cliente")
 @RestController
 @RequestMapping("api/v1/clientes")
 @RequiredArgsConstructor
@@ -25,6 +30,18 @@ public class ClienteControle {
     private final ClienteService clienteService;
     private final UserService userService;
 
+    @Operation(summary = "Criar um novo cliente", description = "Recurso para criar um cliente",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Cliente CPF já cadastrado no sistema",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Dados de entrada Invalidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Usuario sem permissão para acessar o recurso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+
+            })
     @PostMapping
    // @PreAuthorize("hasRole('CLIENTE')")
     public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto dto,
@@ -33,6 +50,22 @@ public class ClienteControle {
         cliente.setUsuario(userService.buscarPorId(userDetails.getId()));
         clienteService.salvar(cliente);
         return ResponseEntity.status(201).body(ClienteMapper.toDto(cliente));
+    }
 
+    @Operation(summary = "localizar um  cliente", description = "Recurso para localizar um cliente",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Cliente não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                     @ApiResponse(responseCode = "403", description = "Usuario sem permissão para acessar o recurso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+
+            })
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ClienteResponseDto> getById(@PathVariable Long id){
+        Cliente cliente = clienteService.buscarPorId(id);
+        return ResponseEntity.ok(ClienteMapper.toDto(cliente));
     }
 }
